66e493e2b0681eae0dfa6241089f334fd652a42e
Harsh-0986 <23f2003845@ds.study.iitm.ac.in>
2025-10-19 22:02:27 +0530

    Setup react router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,13 @@
+import { Outlet } from "react-router-dom";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      {/* Header */}
+      <Outlet />
+      {/* Footer */}
+    </div>
+  );
 }
 
 export default App;
