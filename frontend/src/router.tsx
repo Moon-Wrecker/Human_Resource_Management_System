e915a0b7c5d3e4c1d269eae35aa157b370a763b6
Harsh-0986 <23f2003845@ds.study.iitm.ac.in>
2025-10-20 15:08:35 +0530

    Add Employee Sidebar Structure

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -4,6 +4,8 @@ import App from "@/App";
 
 const Home = lazy(() => import("@/pages/Home"));
 const Login = lazy(() => import("@/pages/Login"));
+const Employee = lazy(() => import("@/layouts/Employee"));
+const EmployeeDashboard = lazy(() => import("@/pages/EmployeeDashboard"));
 
 const router = createBrowserRouter([
   {
@@ -17,6 +19,16 @@ const router = createBrowserRouter([
     ],
   },
   { path: "/login", element: <Login /> },
+  {
+    path: "/employee",
+    element: <Employee />,
+    children: [
+      {
+        index: true,
+        element: <EmployeeDashboard />,
+      },
+    ],
+  },
 ]);
 
 export default router;
